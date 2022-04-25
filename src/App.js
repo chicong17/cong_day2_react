9da38cfba0e3a8a5a92e75dfc566c3889d6bc983
chicong17 <dochicong.fbu@gmail.com>
2022-04-25 15:44:58 +0700

    last update

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Popup from './components/createPopup/Popup'
+import './style/App.scss'
+
+function App() {
+  const [toggle, setToggle] = useState(false)
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+  return (
+    <div className="App">
+      <div>
+        <button onClick={handleToggle} id="buttonClickMe">
+          Click me!
+        </button>
+        {toggle ? (
+          <Popup
+            handleToggle={handleToggle}
+            position="left"
+            idOwner="buttonClickMe"
+          />
+        ) : (
+          true
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
